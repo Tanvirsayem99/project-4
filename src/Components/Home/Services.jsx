3b7bc5199177aab8be669711370b7fd4a0be9aa8
Tanvirsayem99 <107928205+Tanvirsayem99@users.noreply.github.com>
2023-06-12 21:30:33 +0600

    framer motion added

--- src/Components/Home/Services.jsx
+++ src/Components/Home/Services.jsx
@@ -1,4 +1,4 @@
-
+import { motion } from "framer-motion";
 const Services = ({theme}) => {
     
     return (
@@ -10,10 +10,68 @@ const Services = ({theme}) => {
                 <p>-------------------------- O -------------------------</p>
             </div>
 
-      <div className="grid md:grid-cols-3 mx-auto">
-      <div className="bg-white p-5 shadow w-4/6 mx-auto text-center">Academic support</div>
-      <div className="bg-white p-5 shadow w-4/6 mx-auto text-center">Live conversation with Instructors</div>
-      <div className="bg-white p-5 shadow w-4/6 mx-auto text-center">certificate Guaranty</div>
+      <div className="grid md:grid-cols-4 gap-5 mx-auto">
+      <motion.div
+      drag
+      className=" h-56 bg-slate-200 p-5 shadow w-56 mx-auto text-center"
+      animate={{
+
+        rotate: [0, 0, 6, -6, 0],
+        
+      }}
+      transition={{
+        duration: 2,
+        ease: "easeInOut",
+        times: [0, 0.2, 0.5, 0.8, 1],
+        repeat: Infinity,
+        repeatDelay: 0
+      }}
+    > Live conversation with Instructors</motion.div>
+      <motion.div
+      className=" h-56 bg-slate-200 p-5 shadow w-56 mx-auto text-center"
+      animate={{
+        rotate: [0, 0, 6, -6, 0],
+      }}
+      transition={{
+        duration: 2,
+        ease: "easeInOut",
+        times: [0, 0.2, 0.5, 0.8, 1],
+        repeat: Infinity,
+        repeatDelay: 0
+      }}
+    > certificate Guaranty</motion.div>
+      <motion.div
+      className=" h-56 bg-slate-200 p-5 shadow w-56 mx-auto text-center"
+      animate={{
+ 
+        rotate: [0, 0, 6, -6, 0],
+        
+      }}
+      transition={{
+        duration: 2,
+        ease: "easeInOut",
+        times: [0, 0.2, 0.5, 0.8, 1],
+        repeat: Infinity,
+        repeatDelay: 0
+      }}
+    > Academic support</motion.div>
+      <motion.div
+      className=" h-56 bg-slate-200 p-5 shadow w-56 mx-auto text-center"
+      animate={{
+
+        rotate: [0, 0, 6, -6, 0],
+        
+      }}
+      transition={{
+        duration: 2,
+        ease: "easeInOut",
+        times: [0, 0.2, 0.5, 0.8, 1],
+        repeat: Infinity,
+        repeatDelay: 0
+      }}
+    > Every Day Session</motion.div>
+    
+     
       </div>
       
       </>
